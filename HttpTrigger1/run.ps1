05b15b4c0fcc0576797e45a21eb487cbca585824
Ritika259 <risingha@microsoft.com>
2023-05-02 17:55:16 +0530

    az version

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -9,6 +9,10 @@ param($Request, $TriggerMetadata)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
+az version
+az extension add --name azure-devops
+az extension list
+
 az login --identity
 $FunctionApps = az functionapp list | ConvertFrom-Json
 $FunctionApps.Name
